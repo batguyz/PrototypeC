dca219b6c001b19f557a287b864561e5190672cb
Yinka <yinka1911@gmail.com>
2018-12-17 15:08:08 -0500

    added board to navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,9 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/board/">Board</Link>
+      </li>
     </ul>
   </nav>
 )
